f29dbffabdbd025a0e78310165334d2fdff6b25c
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2022-11-21 17:07:39 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,12 @@
+
+function addUser() {
+    player1_name = document.getElementById("???").value;
+    player2_name = document.getElementById("???").value;
+  
+      ???.setItem("player1_name", player1_name);
+      ???.setItem("player2_name", player2_name);
+  
+      window.location = "game_page.html";
+  }
+  
+  
